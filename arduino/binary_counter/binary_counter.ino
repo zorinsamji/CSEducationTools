f9b8691a2c985ee2331056d56fb7d546eeda6d63
zorinsamji <zorin16@gmail.com>
2015-09-24 09:15:05 -0700

    modified binary_counter for iterative circuit development

--- arduino/binary_counter/binary_counter.ino
+++ arduino/binary_counter/binary_counter.ino
@@ -1,58 +1,75 @@
-/**
- * An arduino sketch for counting in binary using an array of four LEDs. 
- */
-
-
-//pin definitions for the LEDs
-
-int COLUMN1 = 0; //CHANGE ME
-int COLUMN2 = 0; //CHANGE ME 
-int COLUMN4 = 0; //CHANGE ME
-int COLUMN8 = 0; //CHANGE ME
-
-int BUTTON = A0; //CHANGE ME
+int COLUMN1 = 11;
+int COLUMN2 = 10; 
+int COLUMN4 = 9; 
+int COLUMN8 = 8; 
 
 int digits[4] = {COLUMN1, COLUMN2, COLUMN4, COLUMN8};
 
+//string to display on Serial Monitor CTRL-SHIFT-M
+
+String stringOne;
+
 void setup() {
 
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  stringOne = String();
+  
   pinMode(COLUMN1, OUTPUT);
   pinMode(COLUMN2, OUTPUT);
   pinMode(COLUMN4, OUTPUT);
   pinMode(COLUMN8, OUTPUT);
-  pinMode(BUTTON, INPUT); 
 
-  Serial.begin(9600);
+  
+  
+ 
+}
+
+// the loop function runs over and over again forever
+void loop2() {
+  digitalWrite(COLUMN1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(COLUMN1, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+
+  digitalWrite(COLUMN2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);              // wait for a second
+  digitalWrite(COLUMN2, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);         // wait for a second
+
 
 }
 
 void loop() {
 
   int y = 0; 
-  int buttonStatus = 0; 
-  
+
   while(1){
 
-    buttonStatus = getButton();
-
-    showNumber(y);
-    y = (y + 1) % 16; 
+    y= showNumber(y,16);
     
-    delay(500);  //CHANGE ME TO COUNT FASTER, 1000 = 1 sec
+
+    Serial.print("Y value");
+    Serial.println(y);
+    
+    delay(500); 
   }
-  
+
 
 }
 
 /**
- * Shows the input number on the binary LED array.
+ * Shows the input number on the binary (mod=16) oo any other base LED array.
  */
-void showNumber(int x){
+int showNumber(int x,int mod){
 
   int i = 0; 
+  int y = x;
 
-  Serial.println(x);
-  
   for(i = 0; i < 4; i++){
 
     if(x % 2 == 0){
@@ -62,18 +79,11 @@ void showNumber(int x){
     }
 
     x = x/2; 
-    
+
   }
   
-}
-int getButton(){
-
-  int val = analogRead(BUTTON);
-
-  if(val > 0){
-    return 1;  
-  }else{
-    return 0; 
-  }
+  
+  y = (y + 1) % mod;    
+  return y;
 }
 
